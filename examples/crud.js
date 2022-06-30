6c51722ada42641772486a57bdf6a679b4c249ce
Manish <msirvi@mcfadyen.com>
2022-06-30 11:24:29 +0530

    added update email action to customer

--- examples/crud.js
+++ examples/crud.js
@@ -1,12 +1,15 @@
-import { createCustomer, getCustomers, getCustomersById} from './customer.js'
+import { createCustomer, getCustomers, getCustomersById,updateCustomerEmail} from './customer.js'
+import { log } from "../logger.js";
+
+// uncomment piece of below code to test the operations, customer id can be retrived from query customer api
 // query customers data
 getCustomers().then(data=> {
     log(data);
 })
 // get customer by id
-getCustomersById("58374859-8ffb-4976-972d-0d4972e7ec21").then(data => {
-    log(data);
-});
+// getCustomersById("58374859-8ffb-4976-972d-0d4972e7ec21").then(data => {
+//     log(data);
+// });
 
 const customerData = {
     firstName: "Create",
@@ -17,8 +20,12 @@ const customerData = {
     countryCode: "DE"
 }
 
-createCustomer(customerData).then(data => {
-    log(data);
-}).catch(error=> {
-    log(error);
-})
+// createCustomer(customerData).then(data => {
+//     log(data);
+// }).catch(error=> {
+//     log(error);
+// })
+
+// updateCustomerEmail("test01@gmail.com","2e5964e9-2a41-4f62-a3f3-2de4cecf7f07").then(data => {
+//     log(data);
+// }).catch(error => log(error));
--- examples/customer.js
+++ examples/customer.js
@@ -1,15 +1,5 @@
 import { apiRoot, projectKey } from "../src/Client.js";
-import { log } from "../logger.js";
 
-export const createCustomer = (customer) => {
-    return apiRoot
-    .withProjectKey({projectKey})
-    .customers()
-    .post({
-        body: createCustomerDraft(customer)
-    })
-    .execute();
-}
 const createCustomerDraft = (customer) => {
     const {
         firstName,
@@ -19,7 +9,7 @@ const createCustomerDraft = (customer) => {
         key,
         countryCode
     } = customer;
-    
+
     return {
         firstName,
         lastName,
@@ -33,27 +23,62 @@ const createCustomerDraft = (customer) => {
         ]
     }
 }
+const createCustomerEmailUpdateDraft = (email, customer) => {
+    return {
+        "version": customer.body.version,
+        "actions": [
+            {
+                "action": "changeEmail",
+                "email": email
+            }
+        ]
+    }
+}
+export const createCustomer = (customer) => {
+    return apiRoot
+        .withProjectKey({ projectKey })
+        .customers()
+        .post({
+            body: createCustomerDraft(customer)
+        })
+        .execute();
+}
+
+export const updateCustomerEmail = (email, ID) => {
+
+    return getCustomersById(ID).then(customer => {
+        return apiRoot
+            .withProjectKey({ projectKey })
+            .customers()
+            .withId({ ID })
+            .post({
+                body: createCustomerEmailUpdateDraft(email, customer)
+            })
+            .execute();
+    })
+
+}
 export const getCustomers = () => {
     return apiRoot
-    .withProjectKey({projectKey})
-    .customers()
-    .get({
-        // we can query arguments like limit offset and conditional query predicates
-        // you can uncomment args to try
-        queryArgs: {
-            // limit: 1,
-            // offset: 1,
-            // where: "lastName=\"Doe\"",
-            sort: "firstName desc"
-        }
-    })
-    .execute();
+        .withProjectKey({ projectKey })
+        .customers()
+        .get({
+            // we can query arguments like limit offset and conditional query predicates
+            // you can uncomment args to try
+            queryArgs: {
+                // limit: 1,
+                // offset: 1,
+                // where: "lastName=\"Doe\"",
+                sort: "firstName desc"
+            }
+        })
+        .execute();
 }
 export const getCustomersById = (ID) => {
     return apiRoot
-    .withProjectKey({projectKey})
-    .customers()
-    .withId({ID})
-    .get()
-    .execute();
+        .withProjectKey({ projectKey })
+        .customers()
+        .withId({ ID })
+        .get()
+        .execute();
 }
