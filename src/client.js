66fc553b64e4bede17cb1a3b8f33d5940297a741
Manish <msirvi@mcfadyen.com>
2022-07-01 14:10:09 +0530

    added anonymous auth client

--- src/client.js
+++ src/client.js
@@ -7,6 +7,7 @@ import fetch from "node-fetch";
 
 const projectKey = process.env.CTP_PROJECT_KEY;
 
+// Client Credentials authorization flow
 const getClient = () => {
     const authMiddlewareOptions = createAuthForClientCredentialsFlow({
         host: process.env.CTP_AUTH_URL,
@@ -26,7 +27,7 @@ const getClient = () => {
     })
     return client;
 };
-
+// Password authorizaion flow
 const meClient = () => {
     const passwordAuthMiddlewareOptions = {
         host: process.env.CTP_AUTH_URL,
@@ -52,7 +53,30 @@ const meClient = () => {
         .build();
     return client;
 }
+// Token for Anonymous session, can be used to create anonymous cart and order
+const anonymousClient = () => {
+    const anonymousAuthMiddlewareOptions = {
+        host: process.env.CTP_AUTH_URL,
+        projectKey,
+        credentials: {
+            clientId: process.env.CTP_CLIENT_ID,
+            clientSecret: process.env.CTP_CLIENT_SECRET
+        },
+        fetch
+    };
+    const httpMiddlewareOptions = {
+        host: process.env.CTP_API_URL,
+        fetch,
+    };
+
+    const client = new ClientBuilder()
+        .withAnonymousSessionFlow(anonymousAuthMiddlewareOptions)
+        .withHttpMiddleware(httpMiddlewareOptions)
+        .build();
+    return client;
+}
 export const apiRoot = createApiBuilderFromCtpClient(getClient());
 export const meApiRoot = createApiBuilderFromCtpClient(meClient());
+export const anonymousApiRoot = createApiBuilderFromCtpClient(anonymousClient());
 const _projectKey = projectKey;
 export { _projectKey as projectKey };
