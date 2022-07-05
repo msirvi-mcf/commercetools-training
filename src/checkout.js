a48082f75aa3b3658c55370a2caecd594135fb08
Manish <msirvi@mcfadyen.com>
2022-07-05 13:14:52 +0530

    moved cart functions to cart.js

--- src/checkout.js
+++ src/checkout.js
@@ -10,29 +10,6 @@ export const customerSignin = (userDetails) => {
         })
         .execute();
 }
-// export const createCart = async(customerId) => {
-//     const customer = await apiRoot.withProjectKey({projectKey}).customers().withId({customerId}).get().execute();
-//     return apiRoot
-//     .withProjectKey({ projectKey }).carts().post({
-//         body: {
-//           currency: "EUR",
-//           country: "DE",
-//           customerId: customer.body.id,
-//           customerEmail: customer.body.email,
-//           shippingAddress: customer.body.addresses.find(address => address.id == customer.body.defaultShippingAddressId),
-//           inventoryMode: "ReserveOnOrder",
-//           deleteDaysAfterLastModification: 90
-//         }
-//     }).execute();
-// }
-// export const getCartById = (ID) => {
-//     return apiRoot
-//         .withProjectKey({ projectKey })
-//         .carts()
-//     .withId({ ID })
-//     .get()
-//     .execute();
-// }
 export const addDiscountCode = async (cartId, discountCode) => {
     getCartById(cartId).then((cart) => {
         return apiRoot
